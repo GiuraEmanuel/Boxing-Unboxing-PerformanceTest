25eda281053c12c5477f691394dc2752ae0b07fc
giura emanuel <emanuel_sb_96@yahoo.com>
2021-03-31 15:21:34 +0300

    update

---
+++ Boxing-Unboxing/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Boxing_Unboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Boxing();
+            UnBoxing();
+        }
+
+        private static void Boxing()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            int j = 15;
+            stopwatch.Start();
+
+            for (int i = 0; i < 100000; i++)
+            {
+                object k = j;
+                Console.WriteLine(k);
+            }
+
+            stopwatch.Stop();
+
+            TimeSpan timeSpan = stopwatch.Elapsed;
+            string elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds,
+            timeSpan.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+        }
+
+        private static void UnBoxing()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            object j = 123;
+            int k;
+
+            stopwatch.Start();
+
+            for (int i = 0; i < 100000; i++)
+            {
+                k = (int)j;
+                Console.WriteLine(k);
+            }
+
+            stopwatch.Stop();
+
+            TimeSpan timeSpan = stopwatch.Elapsed;
+            string elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds,
+            timeSpan.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+        }
+    }
+}
